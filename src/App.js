8e23b6c2359e0cbf2ed6583c8196892815dac02f
Renee Sarley <reneesarley@gmail.com>
2018-11-09 13:21:37 -0800

    add landing component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
+import Landing from './components/landing';
 
 class App extends Component {
   render() {
     return (
       <div className= "App">
-        <h1>Test</h1>
+        <Landing/>
       </div>
     );
   }
---
+++ src/components/landing.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Landing = () => {
+  return(
+    <div className="landing">
+      <h1>Renee Sarley</h1>
+    </div>
+  )
+}
+
+export default Landing;
