a937f12869bdfa30b6f4fe5f07b92265c2c097ea
Renee Sarley <reneesarley@gmail.com>
2018-11-09 14:58:53 -0800

    update video;

--- src/components/structure/Landing.js
+++ src/components/structure/Landing.js
@@ -6,8 +6,8 @@ import treeVid5Sec from './treeVid5Sec.mp4';
 const Landing = () =>{
   return(
     <div className='landing'>
-      <video autoplay="" loop="" muted="" data-reactid=".0.1.0.0">
-        <source type="video/mp4" data-reactid=".0.1.0.0.0" src="img/treeVid5Sec.mp4"/>
+      <video autoplay="" loop={ true } muted={ true }>
+        <source type="video/mp4" src="img/tree.mp4"/>
       </video>
       <h1 className='logo' id='firstname'>Renee</h1>
       <h1 className='logo' id='lastname'>Sarley</h1>
