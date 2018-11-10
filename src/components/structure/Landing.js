1e2384e75ba413f7b5f9facf306246eb8c748b62
Renee Sarley <reneesarley@gmail.com>
2018-11-09 16:11:53 -0800

    add h2 to landing component

--- src/components/structure/Landing.js
+++ src/components/structure/Landing.js
@@ -9,8 +9,10 @@ const Landing = () =>{
       <video autoplay="" loop={ true } muted={ true }>
         <source type="video/mp4" src="img/tree.mp4"/>
       </video>
-      <h1 className='logo' id='firstname'>Renee</h1>
-      <h1 className='logo' id='lastname'>Sarley</h1>
+      <div className='logo'>
+        <h1>Renee<br/><span id='lastname'>Sarley</span></h1>
+        <h2>Full Stack Developer</h2>
+      </div>
     </div>
   )
 }
