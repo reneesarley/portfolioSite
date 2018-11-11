8fc56be280862b319f0162955e67237f9d26c010
Renee Sarley <reneesarley@gmail.com>
2018-11-11 08:54:09 -0800

    add contact component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Landing from './components/structure/Landing';
 import Projects from './components/projects/Projects';
 import About from './components/about/About';
+import Contact from './components/contact/Contact'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Landing/>
         <Projects/>
         <About/>
+        <Contact/>
       </div>
     );
   }
---
+++ src/components/contact/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Contact.css';
+
+const Contact = ()=>{
+  return(
+    <div className='contact'>
+    This is the contact section
+    </div>
+  )
+}
+
+export default Contact;
