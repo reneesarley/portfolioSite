b1208a1dbfb6f25de1a96f16209cba59c1a2008d
Renee Sarley <reneesarley@gmail.com>
2018-11-11 11:52:42 -0800

    update tech stack section"

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -13,7 +13,80 @@ const About = () =>{
       </p>
       <div id='techStack'>
         <h4>Tech stack</h4>
-
+        <h5>Languages</h5>
+        <div className="row skillsRow">
+          <div className="col s4 skill">JavaScript </div>
+          <div className="col s4 skill">
+            C#
+          </div>
+          <div className="col s4 skill">
+            SQL
+          </div>
+          <div className="col s4 skill">
+            TypeScript
+          </div>
+          <div className="col s4 skill">
+            HTML
+          </div>
+          <div className="col s4 skill">
+            CSS
+          </div>
+        </div>
+        <h5>Frameworks & Libraries</h5>
+        <div className="row skillsRow">
+          <div className="col s4 skill">
+            Angular
+          </div>
+          <div className="col s4 skill">
+            React
+          </div>
+          <div className="col s4 skill">
+            Redux
+          </div>
+          <div className="col s4 skill">
+            jQuery
+          </div>
+          <div className="col s4 skill">
+            Bootstrap
+          </div>
+          <div className="col s4 skill">
+            Materialize
+          </div>
+        </div>
+        <h5>Principles & Systems</h5>
+        <div className="row skillsRow">
+          <div className="col s6 skill">
+            TDD/BDD
+          </div>
+          <div className="col s6 skill">
+            Unit Testing
+          </div>
+          <div className="col s6 skill">
+            Linting
+          </div>
+          <div className="col s6 skill">
+            Restful Routing
+          </div>
+          <div className="col s12 skill">
+            Data Normalization
+          </div>
+        </div>
+          <h5>Applications & Tools</h5>
+        <div className="row skillsRow">
+          <div className="col s4 skill">Git </div>
+          <div className="col s4 skill">
+            Webpack
+          </div>
+          <div className="col s4 skill">
+            SQL Server
+          </div>
+          <div className="col s6 skill">
+            myPhpAdmin
+          </div>
+          <div className="col s6 skill">
+            Firebase
+          </div>
+        </div>
       </div>
     </div>
   )
