eb1697dae7b857f4e6a5f49f4ca21f1b2f0f3df3
Renee Sarley <reneesarley@gmail.com>
2018-11-11 12:11:54 -0800

    update bg image of tech stack"

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Contact.css';
+import '../../master.css';
 
 const Contact = ()=>{
   return(
-    <div className='contact'>
-    This is the contact section
+    <div className='contac section'>
+      <h3>Contact</h3>
+      <div className='row'>
+        <div className ='col s4'> Email</div>
+        <div className ='col s4'> Github</div>
+        <div className ='col s4'> LinkedIn</div>
+      </div>
     </div>
   )
 }
