545888010f9d721da77cc7cef3614203db7f3501
Renee Sarley <reneesarley@gmail.com>
2018-11-11 17:26:43 -0800

    position message button

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -14,8 +14,8 @@ const Contact = ()=>{
           <br/>
           LinkedIn <a href='https://www.linkedin.com/in/reneesarley/' target='_blank'><span className='contactType'>reneesarley</span></a>
         </div>
-        <div className ='col s8 row'>
-          <div className= 'col s9'>Know of a <span className='nonProfit'>non-profit</span> that would benefit from <span className='nonProfit'>pro-bono</span> web design, software development or data managment? Let me know! I am always looking for small side projects that will help me develop my skills and benefit my community.</div><div className='col s3'><button className='waves-light btn-small'>Message Me</button></div>
+        <div className ='col s8 row nonProfitSection'>
+          <div className= 'col s9 flow-text'>Know of a <span className='nonProfit'>non-profit</span> that would benefit from <span className='nonProfit'>pro-bono</span> web design, software development or data managment?<br/>Let me know! I am always looking for small side projects that will help me develop my skills and benefit my community.</div><div className='col s3 buttonContainer'><button className='waves-light btn-small messageBtn'>Message Me</button></div>
         </div>
       </div>
     </div>
