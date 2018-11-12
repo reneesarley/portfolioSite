5a74f0869113d3d1a176e1d8a9d4c465fe95d0a6
Renee Sarley <reneesarley@gmail.com>
2018-11-12 10:18:56 -0800

    make ProjectCard stateful, add eventlisteners

--- src/components/projects/ProjectCard.js
+++ src/components/projects/ProjectCard.js
@@ -1,20 +1,42 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import './Projects.css';
 
-const ProjectCard = (props) => {
+class ProjectCard extends Component {
+  constructor(props) {
+   super(props);
+   this.state = {
+      hover: false
+    };
+  this.hoverOn = this.hoverOn.bind(this);
+  this.hoverOff = this.hoverOff.bind(this);
+  }
+
+ hoverOn(){
+  console.log('hover on');
+  this.setState({ hover: true })
+ }
+
+  hoverOff(){
+   console.log('hover off');
+   this.setState({ hover: false })
+  }
+
+ render(){
   return(
-    <div className='col m4 projectBlock'>
+    <div className={ this.state.hover ? "col m12 projectBlock" : "col m4 projectBlock"}
+        onMouseEnter={this.hoverOn} onMouseLeave={this.hoverOff}>
       <div className='projectCard'>
-        <h4>{props.name}</h4>
+        <h4>{this.props.name}</h4>
         <div className='description'>
-        {props.description}
+        {this.props.description}
         </div>
         <div className='techUsed'>
         </div>
       </div>
     </div>
-  )
+    )
+  }
 }
 
 ProjectCard.propTypes = {
