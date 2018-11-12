318d1f8d30e5fd2cc73bdd1cfce19d6cd8e40a3b
Renee Sarley <reneesarley@gmail.com>
2018-11-11 16:45:35 -0800

    add contact types to contact section

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -4,12 +4,19 @@ import '../../master.css';
 
 const Contact = ()=>{
   return(
-    <div className='contac section' id='contactSection'>
+    <div className='contact section' id='contactSection'>
       <h3>Contact</h3>
       <div className='row'>
-        <div className ='col s4'> Email</div>
-        <div className ='col s4'> Github</div>
-        <div className ='col s4'> LinkedIn</div>
+        <div className ='col s4 contactInfo'>
+          Email <span className='contactType'>reneesarley@gmail.com</span>
+          <br/>
+          GitHub <a href='https://github.com/reneesarley' target='_blank'><span className='contactType'>reneesarley</span></a>
+          <br/>
+          LinkedIn <a href='https://www.linkedin.com/in/reneesarley/' target='_blank'><span className='contactType'>reneesarley</span></a>
+        </div>
+        <div className ='col s8 row'>
+          <div className= 'col s9'>Know of a <span className='nonProfit'>non-profit</span> that would benefit from <span className='nonProfit'>pro-bono</span> web design, software development or data managment? Let me know! I am always looking for small side projects that will help me develop my skills and benefit my community.</div><div className='col s3'><button className='waves-light btn-small'>Message Me</button></div>
+        </div>
       </div>
     </div>
   )
