1e5b7b56269c2364d4c566a9afdd0c5f6f085ddf
Renee Sarley <reneesarley@gmail.com>
2018-11-13 15:25:23 -0800

    update font for about section mobile view

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -7,14 +7,14 @@ const About = () =>{
     <div className='about section' id='aboutSection'>
       <h3>About Me</h3>
       <div className='row'>
-        <div className='col s12 m8'>
+        <div className='col s12 m7'>
           <p className= 'flow-text'id='aboutMe'>I am a developer based in the Pacific Northwest. After 5+ years of working in data management and analysis I decided to expand my programming skills and persue a career that would allow me to combine my love of
           <br/><br/>
           By combining self-study with 500+ hours of pair programming experience, I have learned several additional languages, frameworks and programming principles in just a few months.<br/><br/>
           Excited to fully contribute my current abilities to a team while further developing my programing skill set and my understanding of the nuances of software development.<br/>
           </p>
         </div>
-        <div className='col s12 m4'id='techStack'>
+        <div className='col s12 offset-m1 m4'id='techStack'>
           <h4>Technical Skills</h4>
           <h5>Languages</h5>
           <div className="row skillsRow" id='languages'>
